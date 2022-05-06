652b3c1d5a84d7642462a3103ba99f177480928a
humphreyokoth <humpasp@gmail.com>
2022-05-06 06:51:37 +0300

    basics practice

---
+++ array.js
@@ -0,0 +1,27 @@
+// const fruits = ["banana","orange","apple"]
+
+// //fruits.push ("lemon")
+
+
+// function myfruits(){
+// let numOfFruits=0;
+
+// for(let i = 0;i<numOfFruits.length;i++){
+//     sum+=numOfFruits[i]
+
+// }
+// return numOfFruits;
+// }
+// myfruits();
+// console.log(myfruits())
+// const points = [40, 100, 1, 5, 25, 10];
+
+// for (let i = points.length -1; i > 0; i--) {
+//   console.log(points[i]);
+//   let j = Math.floor(Math.random() * i)
+//   let k = points[i]
+//   points[i] = points[j]
+//   points[j] = k
+// }
+let x = Math.round(9.5)
+console.log(x)
