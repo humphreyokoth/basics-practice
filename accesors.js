0bc15e6b26c2e09c452e41aba6df0d023ef79126
humphreyokoth <humpasp@gmail.com>
2022-05-19 10:16:18 +0300

    iterables

--- accesors.js
+++ accesors.js
@@ -7,4 +7,19 @@ Object.defineProperty(obj,"reset",{
     }
 })
 obj.reset;
-console.log(obj.counter);
+//console.log(obj.counter);
+
+// ITERABLES
+function myNumbers(){
+
+    let n = 0;
+    return{
+        next:function(){
+            n+=10
+            return {value:n, done:false};
+        }
+    };
+}
+
+const n = myNumbers();
+console.log(n.next().value);
