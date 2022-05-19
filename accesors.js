9cf6d11402723f5d980b9548036aaf3d74edfa16
humphreyokoth <humpasp@gmail.com>
2022-05-19 11:10:31 +0300

    function invocation

--- accesors.js
+++ accesors.js
@@ -22,4 +22,30 @@ function myNumbers(){
 }
 
 const n = myNumbers();
-console.log(n.next().value);
+//console.log(n.next().value);
+
+// function mySum(){
+//     let arr=[1,2,3,4,5]
+//     let sum =""
+//     for(let i=0;i<arr.length;i++)
+
+//     return sum+=arr[i];
+// }
+
+// mySum();
+// console.log(arr[i]);
+
+
+let ar =[1000000001, 1000000002 ,1000000003, 1000000004 ,1000000005]
+function aVeryBigSum(ar) {
+    // Write your code here
+    let sum=0;
+    for(let i=0;i<ar.length;i++){
+        sum = sum+ar[i]
+    }
+    
+return sum
+}
+ aVeryBigSum(ar);
+ console.log(aVeryBigSum(ar));
+ 
