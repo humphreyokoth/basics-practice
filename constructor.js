ded89c0110edfecd73d355987132a3ccdf899a19
humphreyokoth <humpasp@gmail.com>
2022-05-23 05:53:58 +0300

    inheritance

--- constructor.js
+++ constructor.js
@@ -1,15 +1,39 @@
-class Car {
+// class Car {
 
-    constructor(name,year){
-        this.name =name;
-        this.year = year;
+//     constructor(name,year){
+//         this.name =name;
+//         this.year = year;
+//     }
+//     age(){
+//         let date = new Date();
+//         return date.getFullYear() - this.year;
+//     }
+// }
+
+// let myCar = new Car("Ford",2014);
+
+//console.log("My car is " +  myCar.age() +  "years old");
+
+// Class Inheritance
+class Car{
+    constructor(brand){
+        this.carname = brand;
     }
-    age(){
-        let date = new Date();
-        return date.getFullYear() - this.year;
+    present(){
+        return "i have a " + this.carname;
+    }
+}
+class Model extends Car {
+
+    constructor(brand,mod){
+        super(brand);
+        this.model =mod;
+
+    }
+    show(){
+        return this.present() + ',it is a ' + this.model;
     }
 }
 
-let myCar = new Car("Ford",2014);
-
-console.log("My car is " +  myCar.age() +  "years old");
+let myCar1 = new Model("Ford","Mustang");
+console.log(myCar1.show());
