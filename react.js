43071f18a3a70c2a52ce2c12aae37c1a384ba372
humphreyokoth <humpasp@gmail.com>
2022-05-25 06:43:14 +0300

    react components

---
+++ react.js
@@ -0,0 +1,36 @@
+/** 
+*Simple component with JSX syntax to input data passed into the componentt to be
+accessed by render() via this.props.
+*/ 
+
+class HelloMessage extends React.Component{
+
+    render(){
+        return <div>Hello{this.props.name}</div>
+    }
+}
+
+root.render(<HelloMessage name ="Taylor"/>);
+
+/**
+ * Astateful component when a components state changes data
+ * markup
+ * 
+ * 
+ */
+
+class Timer extends React.Component{
+
+    constructor(props){
+super(props);
+this.state = {seconds:0};
+}
+tick(){
+    this.setState(state =>({
+        seconds:state.seconds +1
+    }));
+}
+}
+componentDidMount(){
+    this.interval = setInterval(()=> this.tick(),1000);
+}
