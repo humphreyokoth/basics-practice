8938236ff48ecf2ef50493f467fe101d2d2e1201
humphreyokoth <humpasp@gmail.com>
2022-06-27 20:10:20 +0300

    objects

--- oddNumber.js
+++ oddNumber.js
@@ -2,4 +2,13 @@ const numbers =[8,19,5,6,14,9,13];
 
 const odds = numbers.filter((num)=> num%2 ===1);
 
-console.log(odds);
+//console.log(odds);
+
+let person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age     : 50,
+    eyeColor  : "blue"
+  };
+
+  console.log(person.age);
