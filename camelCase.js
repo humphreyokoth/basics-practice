581e9a1356b42c6818f9ce618e99c70182bf5b5b
humphreyokoth <humpasp@gmail.com>
2022-07-18 22:09:48 +0300

    Feat:camel case  string

--- camelCase.js
+++ camelCase.js
@@ -10,3 +10,16 @@ function camelcase(s) {
  }
  
  console.log(camelcase("saveChangesInTheEditor"));
+
+ 
+ function camelcase(s) {
+  let count = 1;
+  for(let i=0;i<s.length;i++){
+      if(s[i] == s[i].toUpperCase()){
+          count++;
+      }
+  }
+  return count;
+}
+console.log(camelcase("saveChangesInTheEditor"));
+camelcase('saveChangesInTheEditor');
