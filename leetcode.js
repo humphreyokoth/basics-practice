8b585c0f9ec9e4a6e6a9f13ba63eb0e0768739b4
humphreyokoth <humpasp@gmail.com>
2023-06-17 06:55:25 +0300

    Feat:length of longest string

--- leetcode.js
+++ leetcode.js
@@ -23,4 +23,24 @@ const twoSum = (nums, target) => {
   const result = twoSum(nums, target);
   console.log("Result:", result);
   
-  
+
+
+/**
+ /**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let max = 0
+    let begin = 0
+    let map = {}
+
+    for(let end = 0;end<s.length;end ++){
+        if(map[s[end]] !== undefined && map[s[end]]>= begin){
+            begin = map[s[end]] +1
+        }
+        map[s[end]] = end
+        max = Math.max(max,end-begin +1)
+    }
+    return max
+};
